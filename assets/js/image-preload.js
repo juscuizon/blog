711967ea419cb40836133beda83ee6dda54d311b
WFJ Cuizon <justincuizon@gmail.com>
2017-07-25 07:56:02 +0800

    Fixed multiple onload functions for image preload.

--- assets/js/image-preload.js
+++ assets/js/image-preload.js
@@ -15,21 +15,55 @@ function removeClass(ele, cls) {
 }
 
 //onload function
-function blogImages() 
+
+function imagePreload() 
 {
     var load = document.getElementsByClassName('featured-image');
     for (var i = 0; i < load.length; i++)
     {
-        if (!hasClass(load[i], "preload")) { addClass(load[i], "preload"); } 
-        else { removeClass(load[i], "preload"); }
+       if (!hasClass(load[i], "preload")) 
+       { 
+           addClass(load[i], "preload"); 
+       } 
+       else 
+       { 
+           removeClass(load[i], "preload"); 
+       }
     }
 
     var load1 = document.getElementsByClassName('featured-image-small');
     for (var i = 0; i < load1.length; i++)
     {
-        if (!hasClass(load1[i], "preload")) { addClass(load1[i], "preload"); } 
-        else { removeClass(load1[i], "preload"); }
+        if (!hasClass(load1[i], "preload")) 
+        {       
+            addClass(load1[i], "preload"); 
+        } 
+        else 
+        { 
+            removeClass(load1[i], "preload"); 
+        }
     }
+
+    var load2 = document.getElementsByClassName('image')[0];
+    if (!hasClass(load2, "preload")) 
+    {
+        addClass(load2, "preload");
+    } 
+    else 
+    {
+        removeClass(load2, "preload");
+    }
+
+    var load3 = document.getElementsByClassName('small-image')[0];
+    if (!hasClass(load3, "preload")) 
+    {
+        addClass(load3, "preload");
+    } 
+    else 
+    {
+        removeClass(load3, "preload");
+    }
+
 }
 
-window.onload = blogImages;
+window.onload=imagePreload;
